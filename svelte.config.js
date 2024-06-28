ff07355744146b378ebd4675f12ccc3524ff82c8
Amit Prasad <mail@amitprasad.dev>
2024-06-28 19:30:44 -0400

    Revert to adapter-static

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import { preprocessMeltUI } from "@melt-ui/pp";
 import sequence from "svelte-sequential-preprocessor";
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config}*/
 const config = {
